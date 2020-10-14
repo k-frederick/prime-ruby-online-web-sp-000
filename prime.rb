c2be6615295e663eb9c713a8ddaedc00efb98619
Kristina Frederick <kristinafrederick@icloud.com>
2020-10-14 00:12:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,15 @@
 def prime?(num)
-  n = 2
-while n < num
-    return false if num % n == 0 #|| return false if num < 0
-    n += 1
-    end
-  true
-end
+  (2..(num - 1)).each do |n|
+     return false if num % n == 0
+   end
+   true
+ end
+
+  #n = 2
+#    return false if num % n == 0 #|| return false if num < 0
+#    n += 1
+#    end
+#  true
+#end
 #end
 #end
